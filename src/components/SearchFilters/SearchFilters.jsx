97716b4ce81406714777d8acc19af54f513d2118
Zoe He <hckevin211@gmail.com>
2022-10-13 01:04:25 +1100

    feat: add search and filters

--- src/components/SearchFilters/SearchFilters.jsx
+++ src/components/SearchFilters/SearchFilters.jsx
@@ -1,56 +1,46 @@
-import {
-    Background,
-    InputDiv,
-    Input,
-    ImgBtn,
-    Ul,
-    Li,
-    A,
-    Polygon,
-} from "./SearchFilters.styles";
+import { Background, InputDiv, Input, ImgBtn, Ul, Li, A, Polygon } from "./SearchFilters.styles";
 
 const SearchFilters = () => {
-    return(
-        
-        <Background>
-            <InputDiv>
-                <Input type="text" placeholder="Search for needs"></Input>
-                <ImgBtn src="/src/images/search_logo.png"></ImgBtn>
-            </InputDiv>
-            <Ul>
-                <Li>
-                    <A href="#home">
-                        Category
-                        <Polygon src="/src/images/Polygon.png"></Polygon>
-                    </A>
-                </Li>
-                <Li>
-                    <A href="#news">
-                        50km Hobart TAS & remotely
-                        <Polygon src="/src/images/Polygon.png"></Polygon>
-                    </A>
-                </Li>
-                <Li>
-                    <A href="#news">
-                        Price
-                        <Polygon src="/src/images/Polygon.png"></Polygon>
-                    </A>
-                </Li>
-                <Li>
-                    <A href="#news">
-                        Offers
-                        <Polygon src="/src/images/Polygon.png"></Polygon>
-                    </A>
-                </Li>
-                <Li>
-                    <A href="#news">
-                        Preference Sort
-                        <Polygon src="/src/images/Polygon.png"></Polygon>
-                    </A>
-                </Li>
-            </Ul>
-        </Background>
-    );
+  return (
+    <Background>
+      <InputDiv>
+        <Input type="text" placeholder="Search for needs"></Input>
+        <ImgBtn src="/src/images/search_logo.png"></ImgBtn>
+      </InputDiv>
+      <Ul>
+        <Li>
+          <A href="#home">
+            Category
+            <Polygon src="/src/images/Polygon.png"></Polygon>
+          </A>
+        </Li>
+        <Li>
+          <A href="#news">
+            50km Hobart TAS & remotely
+            <Polygon src="/src/images/Polygon.png"></Polygon>
+          </A>
+        </Li>
+        <Li>
+          <A href="#news">
+            Price
+            <Polygon src="/src/images/Polygon.png"></Polygon>
+          </A>
+        </Li>
+        <Li>
+          <A href="#news">
+            Offers
+            <Polygon src="/src/images/Polygon.png"></Polygon>
+          </A>
+        </Li>
+        <Li>
+          <A href="#news">
+            Preference Sort
+            <Polygon src="/src/images/Polygon.png"></Polygon>
+          </A>
+        </Li>
+      </Ul>
+    </Background>
+  );
 };
 
 export default SearchFilters;
--- src/components/SearchFilters/SearchFilters.styles.jsx
+++ src/components/SearchFilters/SearchFilters.styles.jsx
@@ -1,82 +1,82 @@
 import styled from "styled-components";
 
 export const Background = styled.div`
-    width: 1920px;
-    height: 90px;
-    margin: auto;
+  width: 1920px;
+  height: 90px;
+  margin: auto;
 
-    border-top: 1px solid #D2D2D2;
+  border-top: 1px solid #d2d2d2;
 
-    display: flex;
-    flex-direction: row;
-    justify-content: flex-start;
-    align-items: center;
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: center;
 `;
 
 export const InputDiv = styled.div`
-    width: 350px;
-    height: 56px;
+  width: 350px;
+  height: 56px;
 
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-    align-items: center;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
 
-    border: 2px solid #D2D2D2;
-    border-radius: 94px;
+  border: 2px solid #d2d2d2;
+  border-radius: 94px;
 `;
 
 export const Input = styled.input`
-    width: 250px;
+  width: 250px;
 
-    ::placeholder {
-        color: #b4b4b4;
-    }
+  ::placeholder {
+    color: #b4b4b4;
+  }
 
-    border: none;
-    outline: none;
+  border: none;
+  outline: none;
 
-    font-family: 'Inter';
-    font-style: normal;
-    font-weight: 500;
-    font-size: 20px;
-    line-height: 24px;
-    letter-spacing: -0.01em;
+  font-family: "Inter";
+  font-style: normal;
+  font-weight: 500;
+  font-size: 20px;
+  line-height: 24px;
+  letter-spacing: -0.01em;
 `;
 
 export const ImgBtn = styled.img`
-    width: 30px;
+  width: 30px;
 `;
 
 export const Ul = styled.ul`
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-    align-items: center;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
 
-    list-style: none;
+  list-style: none;
+  margin-left: 90px;
 `;
 
 export const Li = styled.li`
-    margin-right: 80px;
+  margin-right: 80px;
 
-    font-family: 'Inter';
-    font-style: normal;
-    font-weight: 600;
-    font-size: 20px;
-    line-height: 24px;
-    letter-spacing: -0.01em;
+  font-family: "Inter";
+  font-style: normal;
+  font-weight: 600;
+  font-size: 20px;
+  line-height: 24px;
+  letter-spacing: -0.01em;
 
-    color: #F77F00;
+  color: #f77f00;
 `;
 
 export const A = styled.a`
-    color: #F77F00;
-    text-decoration: none;
+  color: #f77f00;
+  text-decoration: none;
 `;
 
 export const Polygon = styled.img`
-    width: 15px;
-    padding: 0 15px;
-    
+  width: 15px;
+  padding: 0 15px;
 `;
