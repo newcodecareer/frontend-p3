8d4b252d049963641ecd14344c0eb8c254be4948
icegerb <jonreb.k@hotmail.com>
2022-10-13 01:12:59 +1000

    feat: add simple map

--- src/components/BrowseNeeds/NeedsMap/NeedsMap.jsx
+++ src/components/BrowseNeeds/NeedsMap/NeedsMap.jsx
@@ -1,7 +1,22 @@
-import { NeedsMapDiv } from "./NeedsMap.styles";
+import { NeedsMapDiv, StyledMapContainer } from "./NeedsMap.styles";
+import { TileLayer, Marker, Popup } from 'react-leaflet'
 
 const NeedsMap = () => {
-  return <NeedsMapDiv>Map</NeedsMapDiv>;
+  return (
+    <NeedsMapDiv>
+      <StyledMapContainer className='mapSize' center={[-27.5, 153]} zoom={11} scrollWheelZoom={true}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[-27.47, 153.02]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </StyledMapContainer>
+    </NeedsMapDiv>
+  )
 };
 
 export default NeedsMap;
--- src/components/BrowseNeeds/NeedsMap/NeedsMap.styles.jsx
+++ src/components/BrowseNeeds/NeedsMap/NeedsMap.styles.jsx
@@ -1,8 +1,14 @@
 import styled from "styled-components";
+import { MapContainer } from 'react-leaflet'
 
 export const NeedsMapDiv = styled.div`
   width: 65%;
   height: calc(100vh - 3rem);
   margin: 1rem 0;
   padding-bottom: 1.5rem;
+`;
+
+export const StyledMapContainer = styled(MapContainer)`
+  width: 100%;
+  height: 100%;
 `;
