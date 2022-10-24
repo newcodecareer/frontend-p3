107cf5781c6a167c87aaf344032be6cb40505cd6
jeff <kuokamhung@yahoo.com.hk>
2022-10-24 21:32:10 +1100

    fixed: change style to style-component

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -1,79 +1,141 @@
-import "./categories.css";
-import brust from "../../image/brust.png";
-import lock from "../../image/lock.png";
-import short from "../../image/short.png";
-import removal from "../../image/removal.png";
-import builder from "../../image/builder.png";
-import electrician from "../../image/electrican.png";
-import plumber from "../../image/plumber.png";
-import handyman from "../../image/handyman.png";
-import fenceing from "../../image/fenceing.png";
-import painting from "../../image/painting.png";
-import gardening from "../../image/gardening.png";
-import cleaning from "../../image/cleaning.png";
-
-const categoriesOne = [
-  { image: brust, linkLabel: "Burst Pipe", href: "brust" },
-  { image: lock, linkLabel: "Locked Out", href: "#" },
-  { image: short, linkLabel: "Short Out", href: "#" },
-  { image: removal, linkLabel: "Removal", href: "#" },
-];
-
-const categoriesTwo = [
-  { image: builder, linkLabel: "Builder", href: "#" },
-  { image: electrician, linkLabel: "Electrician", href: "#" },
-  { image: plumber, linkLabel: "Plumber", href: "#" },
-  { image: handyman, linkLabel: "Handyman", href: "#" },
-];
-
-const categoriesThree = [
-  { image: fenceing, linkLabel: "Fencing", href: "#" },
-  { image: painting, linkLabel: "Painting", href: "#" },
-  { image: gardening, linkLabel: "Gardening", href: "#" },
-  { image: cleaning, linkLabel: "Cleaning", href: "#" },
-];
-
-const listItems = categoriesOne.map((category) => (
-  <div className="card">
-    <img src={category.image} alt="OMG something wrong" />
-    <a href={category.href}>{category.linkLabel}</a>
-  </div>
-));
-
-const listItemsTwo = categoriesTwo.map((category) => (
-  <div className="card">
-    <img src={category.image} alt="OMG something wrong" />
-    <a href={category.href}>{category.linkLabel}</a>
-  </div>
-));
-
-const listItemsThree = categoriesThree.map((category) => (
-  <div className="card">
-    <img src={category.image} alt="OMG something wrong" />
-    <a href={category.href}>{category.linkLabel}</a>
-  </div>
-));
+import { Category, Heading, SubHeading, CardContainer, Card, Img, Text } from "./categories.style";
 
 const Categories = () => {
   return (
-    <div>
-      <div className="categories-0">
-        <h2>Explore categories to see what you need.</h2>
-      </div>
-      <div className="categories-1">
-        <h3>Emergency</h3>
-      </div>
-
-      <ul className="card-container">{listItems}</ul>
-
-      <div className="categories-2">
-        <h3>Most popular</h3>
-      </div>
-
-      <ul className="card-container">{listItemsTwo}</ul>
-      <ul className="card-container">{listItemsThree}</ul>
-    </div>
+    <Category>
+      <Heading>Explore categories to see what you need.</Heading>
+      <SubHeading>Emergency</SubHeading>
+      <CardContainer>
+        <Card>
+          <Img src="/src/images/brust.png" alt="OMG something wrong" />
+          <Text>Burst Pipe</Text>
+        </Card>
+        <Card>
+          <Img src="/src/images/lock.png" alt="OMG something wrong" />
+          <Text>Locked Out</Text>
+        </Card>
+        <Card>
+          <Img src="/src/images/short.png" alt="OMG something wrong" />
+          <Text>Short Out</Text>
+        </Card>
+        <Card>
+          <Img src="/src/images/removal.png" alt="OMG something wrong" />
+          <Text>Removal</Text>
+        </Card>
+      </CardContainer>
+      <SubHeading>Most Popular</SubHeading>
+      <CardContainer>
+        <Card>
+          <Img src="/src/images/builder.png" alt="OMG something wrong" />
+          <Text>Builder</Text>
+        </Card>
+        <Card>
+          <Img src="/src/images/electrican.png" alt="OMG something wrong" />
+          <Text>Electrician</Text>
+        </Card>
+        <Card>
+          <Img src="/src/images/plumber.png" alt="OMG something wrong" />
+          <Text>Plumber</Text>
+        </Card>
+        <Card>
+          <Img src="/src/images/handyman.png" alt="OMG something wrong" />
+          <Text>Handyman</Text>
+        </Card>
+      </CardContainer>
+      <CardContainer>
+        <Card>
+          <Img src="/src/images/fenceing.png" alt="OMG something wrong" />
+          <Text>Fencing</Text>
+        </Card>
+        <Card>
+          <Img src="/src/images/painting.png" alt="OMG something wrong" />
+          <Text>Painting</Text>
+        </Card>
+        <Card>
+          <Img src="/src/images/gardening.png" alt="OMG something wrong" />
+          <Text>Gardening</Text>
+        </Card>
+        <Card>
+          <Img src="/src/images/cleaning.png" alt="OMG something wrong" />
+          <Text>Cleaning</Text>
+        </Card>
+      </CardContainer>
+    </Category>
   );
 };
 
 export default Categories;
+
+// import "./categories.css";
+// import brust from "../../image/brust.png";
+// import lock from "../../image/lock.png";
+// import short from "../../image/short.png";
+// import removal from "../../image/removal.png";
+// import builder from "../../image/builder.png";
+// import electrician from "../../image/electrican.png";
+// import plumber from "../../image/plumber.png";
+// import handyman from "../../image/handyman.png";
+// import fenceing from "../../image/fenceing.png";
+// import painting from "../../image/painting.png";
+// import gardening from "../../image/gardening.png";
+// import cleaning from "../../image/cleaning.png";
+
+// const categoriesOne = [
+//   { image: brust, linkLabel: "Burst Pipe", href: "brust" },
+//   { image: lock, linkLabel: "Locked Out", href: "#" },
+//   { image: short, linkLabel: "Short Out", href: "#" },
+//   { image: removal, linkLabel: "Removal", href: "#" },
+// ];
+
+// const categoriesTwo = [
+//   { image: builder, linkLabel: "Builder", href: "#" },
+//   { image: electrician, linkLabel: "Electrician", href: "#" },
+//   { image: plumber, linkLabel: "Plumber", href: "#" },
+//   { image: handyman, linkLabel: "Handyman", href: "#" },
+// ];
+
+// const categoriesThree = [
+//   { image: fenceing, linkLabel: "Fencing", href: "#" },
+//   { image: painting, linkLabel: "Painting", href: "#" },
+//   { image: gardening, linkLabel: "Gardening", href: "#" },
+//   { image: cleaning, linkLabel: "Cleaning", href: "#" },
+// ];
+
+// const listItems = categoriesOne.map((category) => (
+//   <div className="card">
+//     <img src={category.image} alt="OMG something wrong" />
+//     <a href={category.href}>{category.linkLabel}</a>
+//   </div>
+// ));
+
+// const listItemsTwo = categoriesTwo.map((category) => (
+//   <div className="card">
+//     <img src={category.image} alt="OMG something wrong" />
+//     <a href={category.href}>{category.linkLabel}</a>
+//   </div>
+// ));
+
+// const listItemsThree = categoriesThree.map((category) => (
+//   <div className="card">
+//     <img src={category.image} alt="OMG something wrong" />
+//     <a href={category.href}>{category.linkLabel}</a>
+//   </div>
+// ));
+
+// <div>
+//   <div className="categories-0">
+//     <h2>Explore categories to see what you need.</h2>
+//   </div>
+//   <div className="categories-1">
+//     <h3>Emergency</h3>
+//   </div>
+
+//   <ul className="card-container">{listItems}</ul>
+
+//   <div className="categories-2">
+//     <h3>Most popular</h3>
+//   </div>
+
+//   <ul className="card-container">{listItemsTwo}</ul>
+//   <ul className="card-container">{listItemsThree}</ul>
+// </div>
--- src/components/Categories/categories.style.jsx
+++ src/components/Categories/categories.style.jsx
@@ -1,41 +1,33 @@
-* {
+import styled from "styled-components";
+
+export const Category = styled.div`
   margin: 0;
   padding: 0;
   box-sizing: border-box;
-}
+`;
 
-h2 {
+export const Heading = styled.span`
   font-size: 45px;
-}
+  display: flex;
+  justify-content: center;
+  margin-top: 50px;
+  font-weight: bold;
+`;
 
-h3 {
+export const SubHeading = styled.span`
   color: darkorange;
-}
-
-.categories-0 {
   display: flex;
   justify-content: center;
   margin-top: 50px;
-}
+  font-weight: bold;
+`;
 
-.categories-1 {
+export const CardContainer = styled.div`
   display: flex;
   justify-content: center;
-  margin-top: 50px;
-}
+`;
 
-.categories-2 {
-  display: flex;
-  justify-content: center;
-  margin-top: 50px;
-}
-
-.card-container {
-  display: flex;
-  justify-content: center;
-}
-
-.card {
+export const Card = styled.div`
   border-style: groove;
   padding: 15px;
   margin: 20px;
@@ -45,18 +37,18 @@ h3 {
   flex-direction: column;
   justify-content: center;
   width: 130px;
-}
+`;
 
-img {
+export const Img = styled.img`
   height: 60px;
   padding-left: 15px;
-}
+`;
 
-a {
+export const Text = styled.a`
   padding-top: 5px;
   padding-left: 10px;
   text-decoration: none;
   color: black;
   font-weight: bold;
   text-align: center;
-}
+`;
