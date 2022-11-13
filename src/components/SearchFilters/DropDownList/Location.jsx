5c6af224723a09e11112bc9f41c43c74160bc847
Zoe He <hckevin211@gmail.com>
2022-11-14 02:24:14 +1100

    feat: add drop down list

--- src/components/SearchFilters/DropDownList/Location.jsx
+++ src/components/SearchFilters/DropDownList/Location.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import {
   Background,
   BottomContainer,
@@ -7,14 +8,17 @@ import {
   Btn,
   ApplyBtn,
   Input,
-  RangeScroll,
   Wrap,
   GlobalStyle,
   Shade,
   PopUp,
+  RangeScroll,
+  SelectedValue,
 } from "./Category.styles";
 
 const Location = () => {
+  const [distance, setDistance] = useState("45");
+
   return (
     <Background>
       <GlobalStyle />
@@ -36,8 +40,16 @@ const Location = () => {
         </div>
         <div>
           <Paragraph>DISTANCE</Paragraph>
-          <Wrap>
-            <RangeScroll type="range" min="5" max="100" />
+          <Wrap location_distance>
+            <SelectedValue>{distance} KM</SelectedValue>
+            <RangeScroll
+              type="range"
+              min="5"
+              max="105"
+              step="20"
+              value={distance}
+              onChange={(event) => setDistance(event.target.value)}
+            />
           </Wrap>
         </div>
         <Breakline location></Breakline>
--- src/components/SearchFilters/DropDownList/Offers.jsx
+++ src/components/SearchFilters/DropDownList/Offers.jsx
@@ -12,9 +12,29 @@ import {
   Shade,
 } from "./Category.styles";
 
+import React, { useState } from "react";
 import Switch from "react-switch";
 
 const Offers = () => {
+  const [hideAssigned, setHideAssigned] = useState(false);
+  const [hideOffered, setHideOffered] = useState(false);
+
+  const handleChangeAssigned = () => {
+    if (!hideAssigned) {
+      setHideAssigned(true);
+    } else {
+      setHideAssigned(false);
+    }
+  };
+
+  const handleChangeOffered = () => {
+    if (!hideOffered) {
+      setHideOffered(true);
+    } else {
+      setHideOffered(false);
+    }
+  };
+
   return (
     <Background>
       <GlobalStyle />
@@ -26,7 +46,7 @@ const Offers = () => {
             <Paragraph bk>Available needs only</Paragraph>
             <Paragraph sml>Hide assigned needs</Paragraph>
           </ParagraphBlock>
-          <Switch />
+          <Switch onChange={handleChangeAssigned} checked={hideAssigned} />
         </Wrap>
         <Breakline></Breakline>
         <Wrap offer>
@@ -34,7 +54,7 @@ const Offers = () => {
             <Paragraph bk>Needs with no offers only</Paragraph>
             <Paragraph sml>Hide needs with offers</Paragraph>
           </ParagraphBlock>
-          <Switch />
+          <Switch onChange={handleChangeOffered} checked={hideOffered} />
         </Wrap>
         <Breakline></Breakline>
         <BottomContainer offer>
